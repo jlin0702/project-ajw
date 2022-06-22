dd36d1674cd06c657194e3589fb2e92355265125
Jacky Lin <linj0702@gmail.com>
2022-06-21 20:09:27 -0400

    personalized site

--- app/__init__.py
+++ app/__init__.py
@@ -8,94 +8,51 @@ app = Flask(__name__)
 '''
     dynamic navigation bar
     url: the url for the link, list
-    people: all the people pages, list
     label: what to call the link, list
 '''
 class nav:
-    def __init__(self, url, people, label):
+    def __init__(self, url, label):
         self.url = url
-        self.people = people
         self.label = label
 
 profile_nav = nav(
     ["", "work_edu", "hobbies"],
-    ["amber", "jacky", "william"],
     ["About Me", "Work Experience/Education", "Hobbies"]
 )
 
 @app.route('/')
 def index():
-    return render_template('index.html', nav=profile_nav, title="MLH Fellow", url=os.getenv("URL"))
-
-# for amber page
-@app.route('/amber')
-def amber():
-    return render_template('amber.html', nav=profile_nav, title="Amber", url=os.getenv("URL"))
+    return render_template('index.html', nav=profile_nav, title="Jacky", url=os.getenv("URL"))
 
 # for jacky page
 @app.route('/jacky')
 def jacky():
-    return render_template('jacky.html', nav=profile_nav, title="Jacky", url=os.getenv("URL"))
-
-# for william page
-@app.route('/william')
-def william():
-    return render_template('william.html', nav=profile_nav, title="William", url=os.getenv("URL"))
+    return render_template('jacky.html', nav=profile_nav, title="About Me", url=os.getenv("URL"))
 
 # for work experience/education page
-@app.route('/work_edu/<name>')
-def work_edu(name):
-    profile_title = name.capitalize()
-    job = {
-        "amber": ["Black Stallion Winery"],
-        "jacky": ["Wendy's"],
-        "william": ["U.S. Census Bureau"]
-    }
-    # key: name
+@app.route('/work_edu')
+def work_edu():
+    job = ["Wendy's"]
     # value: 2d list with inner generating new lines
-    job_description = {
-        "amber": [["July 2019 - June 2020"]],
-        "jacky": [["October 2019 - March 2020"]],
-        "william": [["August 2020 - November 2020"]]
-    }
-    education = {
-        "amber": ["University of Pennsylvania", "University of California, Davis"],
-        "jacky": ["University of Kansas", "Northwest High School"],
-        "william": ["University of Chicago", "Stuyvesant High School"]
-    }
-    # key: name
+    job_description = [["October 2019 - March 2020"]],
+    education = ["University of Kansas", "Northwest High School"]
     # value: 2d list with inner generating new lines
-    edu_description = {
-        "amber": [["M.S Computer and Information Technology","Aug 2021 - May 2023"], ["Viticulture and Enology","Sep 2015 - Jun 2018"]],
-        "jacky": [["B.S. Computer Science","Aug 2020 - May 2024"], ["High School", "Aug 2016 - May 2020"]],
-        "william": [["B.S. Computer Science, Minor in Philosophy", "Sept 2020 - June 2024"], ["High School", "Sept 2016 - June 2020"]]
-    }
-    profile_edu = education[name]
-    profile_edu_desc = edu_description[name]
-    profile_job = job[name]
-    profile_job_desc = job_description[name]
-    return render_template("work_edu.html", nav=profile_nav, job=profile_job, job_description=profile_job_desc, education=profile_edu, 
-            edu_description=profile_edu_desc, title=profile_title, url=os.getenv("URL"))
+    edu_description = [["B.S. Computer Science","Aug 2020 - May 2024"], ["High School", "Aug 2016 - May 2020"]]
+    return render_template("work_edu.html", nav=profile_nav, job=job, job_description=job_description, education=education, 
+            edu_description=edu_description, title="Work/Education", url=os.getenv("URL"))
 
 # for work experience/education page
-@app.route('/hobbies/<name>')
-def hobbies(name):
-    profile_title = name.capitalize()
-    hobbies = {
-        "amber": ["Travelling, Photography, Wine, Baking"],
-        "jacky": ["Basketball, Gaming, Driving, Deep discussions"],
-        "william": ["dance, volleyball, gaming, tv series"]
-    }
-    profile_hobby = hobbies[name]
-    photos = {
-        "amber": "amber_hobby.jpg",
-        "jacky": "jacky_hobby.jpg",
-        "william": "william_hobby.jpg"
-    }
-    return render_template("hobbies.html", nav=profile_nav, hobbies=profile_hobby, title=profile_title, photo=photos[name], url=os.getenv("URL"))
+@app.route('/hobbies')
+def hobbies():
+    hobbies = ["Gaming, Driving, Deep discussions"]
+    photo = "jacky_hobby.jpg"
+    return render_template("hobbies.html", nav=profile_nav, hobbies=hobbies, title="Hobbies", photo=photo, url=os.getenv("URL"))
 
 
 # for map page
 @app.route('/map')
 def map():
     return render_template('map.html', nav=profile_nav, title="Map", url=os.getenv("URL"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
