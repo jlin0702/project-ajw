aa8654806d21e445acd61901f654249cbc73c6a0
Jacky Lin <linj0702@gmail.com>
2022-06-24 10:06:36 -0400

    replace tmux with service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-tmux kill-server
 cd /root/project-ajw
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -d 'flask run --host=0.0.0.0'
+systemctl restart myportfolio
