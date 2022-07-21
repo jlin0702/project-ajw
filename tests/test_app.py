87c1135c453503ce1920ab5bfdb237fc6d89a3c7
Jacky Lin <linj0702@gmail.com>
2022-07-21 15:36:42 -0400

    update title

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,7 +14,7 @@ class AppTestCase(unittest.TestCase):
         response = self.client.get('/')
         assert response.status_code == 200
         html = response.get_data(as_text=True)
-        assert '<title>Jacky</title>' in html
+        assert '<title>Jacky Lin</title>' in html
         assert '<img src="./static/img/logo.jpg">' in html
 
     def test_timeline(self):
